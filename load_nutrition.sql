4412b8a2330233db113b614cd07a966c66cfccb8
ismael <ismaelfm_@ciencias.unam.mx>
2018-09-06 13:57:25 -0500

    Adding foreign keys field

--- load_nutrition.sql
+++ load_nutrition.sql
@@ -5,8 +5,8 @@ CREATE TABLE `nutrition` (
   amount float NOT NULL,
   num_data_points int NOT NULL,
   std_error float,
-  source_code text NOT NULL,
-  derivation_code text,
+  source_code int REFERENCES sources(id),
+  derivation_code int REFERENCES derivation_code(id),
   reference_food_id REFERENCES food(id),
   added_nutrient text,
   num_studients int,
@@ -17,7 +17,6 @@ CREATE TABLE `nutrition` (
   upper_error_bound float,
   comments text,
   modification_date text,
-  confidence_code text,
   PRIMARY KEY(food_id, nutrient_id)
 );
 
