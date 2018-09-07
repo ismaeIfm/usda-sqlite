15efac7ad4c6a429c778bf1d40274d8128f2157e
ismael <ismaelfm_@ciencias.unam.mx>
2018-09-06 19:49:43 -0500

    Fixing carbohydrate_factor field name

--- load_food.sql
+++ load_food.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS `food`;
 CREATE TABLE `food` (
-  id char(5) PRIMARY KEY NOT NULL,
-  food_group_id char(4) REFERENCES food_group(id) NOT NULL,
+  ndbno char(5) PRIMARY KEY NOT NULL,
+  food_group_code char(4) REFERENCES food_group(food_group_code) NOT NULL,
   long_desc text NOT NULL DEFAULT '',
   short_desc text NOT NULL DEFAULT '',
   common_names text NOT NULL DEFAULT '',
@@ -13,7 +13,7 @@ CREATE TABLE `food` (
   nitrogen_factor float NOT NULL,
   protein_factor float NOT  NULL,
   fat_factor float NOT NULL,
-  calorie_factor float NOT NULL
+  carbohydrate_factor float NOT NULL
 );
 CREATE INDEX food_short_desc_search_index ON food(short_desc);
 CREATE INDEX food_long_desc_search_index ON food(long_desc);
